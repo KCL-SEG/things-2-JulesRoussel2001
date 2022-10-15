2c285cd29ba477a38b80e24749f18b19ee9280b3
Jules Roussel <k20074057@kcl.ac.uk>
2022-10-15 02:08:09 +0100

    Create SignUpForm

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,11 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import Forms
+from models import Thing
+
+class SignUpForm(froms.modelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description' : forms.Textarea(), 'quantity' : forms.NumberInput()}
